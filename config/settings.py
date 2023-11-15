ca3a58d3f238cfba540730918a0b444ed38a1475
AibekYrysbekov <yrysbekovayba@gmail.com>
2023-11-15 22:56:46 +0600

    created models for the database

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'mystore',
+    'mystore.apps.MystoreConfig',
     'rest_framework',
 ]
 
--- mystore/models.py
+++ mystore/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    brand = models.CharField(max_length=255)
+    images = models.ManyToManyField('Image', related_name='products')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='product_images/')
+
+    def __str__(self):
+        return f"Image {self.id}"
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    products = models.ManyToManyField('Product', related_name='orders')
+    customer_name = models.CharField(max_length=255)
+    shipping_address = models.TextField()
+    status = models.CharField(max_length=255)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order {self.id}"
